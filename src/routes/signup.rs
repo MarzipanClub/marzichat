018d8f3353d979b7440b636b954a3976e566cb53
Jesus Guzman, Jr <jesus@marzipan.club>
2023-07-11 08:34:09 -0700

    added error banner for email validation

--- src/routes/signup.rs
+++ src/routes/signup.rs
@@ -1,7 +1,13 @@
 use {
-    crate::{internationalization::Translations, routes::*, scroll_to_top},
+    crate::{
+        internationalization::Translations,
+        routes::*,
+        scroll_to_top,
+        types::{email::Email, password::Password, validation::Validate, Username},
+    },
     leptos::*,
     leptos_router::*,
+    leptos_use::use_throttle_fn,
 };
 
 #[component]
@@ -11,6 +17,26 @@ pub fn Signup(cx: Scope) -> impl IntoView {
     let (username, set_username) = create_signal(cx, String::default());
     let (password, set_password) = create_signal(cx, String::default());
     let (password_again, set_password_again) = create_signal(cx, String::default());
+
+    let (show_email_error, set_show_email_error) = create_signal(cx, false);
+
+    let check_username_availability = use_throttle_fn(
+        move || {
+            let username: Username = username().into();
+            if username.validate().is_err() {
+                leptos::log!("username is invalid!");
+            }
+        },
+        2000.0,
+    );
+
+    let on_click = move |_| {
+        let email: Email = email().into();
+        if email.validate().is_err() {
+            set_show_email_error(true);
+        }
+    };
+
     scroll_to_top();
     view! { cx,
         <main class="container-sm my-4">
@@ -28,19 +54,26 @@ pub fn Signup(cx: Scope) -> impl IntoView {
                             </div>
                             <div class="form-group-body">
                                 <input class="form-control width-full" type="text" id="email"
-                                    on:input=move |ev| set_email(event_target_value(&ev))
+                                    on:input=move |ev| {  set_email(event_target_value(&ev)); set_show_email_error(false); }
                                     prop:value=email
                                 />
                             </div>
                         </div>
-                        <div class="flash flash-success">"Flash error inside a Box."</div>
+                        <Show when=move || show_email_error() fallback=|_| ()>
+                            <div class="flash flash-error">
+                                <Show when=move || !email().is_empty() fallback=|_| view! { cx, "Please enter an email." } >
+                                    <span class="text-bold">{email()}</span>
+                                    " seems to be invalid or a temporary email. Please fix it or use another email."
+                                </Show>
+                            </div>
+                        </Show>
                         <div class="form-group">
                             <div class="form-group-header">
                                 <label for="username">{t.username()}</label>
                             </div>
                             <div class="form-group-body">
                                 <input class="form-control width-full" type="text" id="username"
-                                    on:input=move |ev| set_username(event_target_value(&ev))
+                                    on:input=move |ev| { set_username(event_target_value(&ev)); check_username_availability(); }
                                     prop:value=username
                                 />
                             </div>
@@ -75,7 +108,7 @@ pub fn Signup(cx: Scope) -> impl IntoView {
                         {t.terms_and_privacy_disclaimer_3()}
                     </p>
                     <div class=" text-right mt-4">
-                        <button class="btn btn-primary">{t.create_free_account()}</button>
+                        <button class="btn btn-primary" on:click=on_click>{t.create_free_account()}</button>
                     </div>
                 </div>
             </div>
